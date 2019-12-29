426cd35463f99abaa1bfde90ba934c4efcaa0b06
Karren Dai Yang <karren@mit.edu>
2019-12-29 17:03:27 -0500

    added classification network

--- models/molimagenet.py
+++ models/molimagenet.py
@@ -7,6 +7,25 @@ from chemprop.models.mpn import MPN
 
 from types import SimpleNamespace
 
+def get_default_args():
+    '''Returns Namespace of arguments for MPN and mol2graph compat'''
+
+    args = SimpleNamespace()
+    args.hidden_size=512
+    args.bias=False
+    args.depth=3
+    args.dropout=0.0
+    args.activation='ReLU'
+    args.undirected=False
+    args.ffn_hidden_size=None
+    args.atom_messages=False
+    args.use_input_features=False
+    args.features_only=False
+    args.no_cache=False
+    args.cuda = True
+
+    return args
+
 class MolImageNet(nn.Module):
     def __init__(self):
         super(MolImageNet, self).__init__()
@@ -25,19 +44,7 @@ class MolImageNet(nn.Module):
 
     def get_chem_net(self):
 
-        args = SimpleNamespace()
-        args.hidden_size=512
-        args.bias=False
-        args.depth=3
-        args.dropout=0.0
-        args.activation='ReLU'
-        args.undirected=False
-        args.ffn_hidden_size=None
-        args.atom_messages=False
-        args.use_input_features=False
-        args.features_only=False
-        args.no_cache=False
-        args.cuda = True
+        args = get_default_args()
 
         feature_net = MPN(args)
         embed_net = nn.Sequential(nn.Linear(512, 512),
@@ -63,3 +70,52 @@ class MolImageNet(nn.Module):
         mol_batch = self.chemnet['embed_net'](mol_batch)
 
         return {'image_embedding': image_batch, 'chem_embedding': mol_batch}
+
+
+class MolImageNetClass(nn.Module):
+    def __init__(self):
+        super(MolImageNetClass, self).__init__()
+
+        self.imagenet = self.get_image_net()
+        self.chemnet = self.get_chem_net()
+        self.classifier = self.get_classifier()
+
+    def get_image_net(self):
+        feature_net = Resnet18Features(in_channels=5)
+
+        return nn.ModuleDict({'feature_net': feature_net})
+
+    def get_chem_net(self):
+
+        args = get_default_args()
+
+        feature_net = MPN(args)
+
+        return nn.ModuleDict({'feature_net': feature_net})
+
+    def get_classifier(self):
+        classifier = nn.Sequential(nn.Linear(1024, 256),
+                                   nn.ReLU(),
+                                   nn.Linear(256, 1),
+            )
+        return classifier
+
+    def forward(self, x):
+        image_batch = x['image']
+
+        if next(self.parameters()).is_cuda:
+            image_batch = image_batch.cuda()
+
+        mol_batch = x['smiles']
+
+        image_batch = self.imagenet['feature_net'](image_batch)
+        image_batch = image_batch.view(image_batch.size(0), 512)
+
+        mol_batch = self.chemnet['feature_net'](mol_batch)
+
+        combined_feats = torch.cat((image_batch, mol_batch), dim=1)
+        logit = self.classifier(combined_feats)
+
+        return {'image_embedding': image_batch, 'chem_embedding': mol_batch, 'logit': logit}
+
+model_dict = {'molimagenet': MolImageNet, 'molimagenetclass': MolImageNetClass}
