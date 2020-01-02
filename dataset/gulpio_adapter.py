93ac21de34b54c7f297675ca657126fea6c8613e
Karren Yang <karren@mit.edu>
2020-01-02 00:16:14 -0500

    gulpio bug fixes

--- dataset/gulpio_adapter.py
+++ dataset/gulpio_adapter.py
@@ -1,11 +1,18 @@
 from gulpio.adapters import AbstractDatasetAdapter
 from gulpio.fileio import GulpIngestor
 
+import pandas as pd
+import numpy as np
+import torchvision.transforms as Transforms
+
+import os
+
 class MolChemAdapter(AbstractDatasetAdapter):
-    def __init__(self, datadir, metadata):
+    def __init__(self, datadir, metafile):
         self.datadir = datadir
         self.metadata = self.load_metadata(metafile)
-        print(metadata.head())
+        print(self.metadata.head())
+        print(len(self))
         self.transforms = Transforms.Compose([Transforms.ToPILImage(),
                                                  ])
     def __len__(self):
@@ -19,27 +26,28 @@ class MolChemAdapter(AbstractDatasetAdapter):
             return pd.read_csv(metafile)
 
     def iter_data(self, slice_element=None):
-        slice_element = slice_element or slice(0, len(self))
-        for idx in list(slice_element):
-            sample = self.metadata.iloc[idx]
+        s = slice_element or slice(0, len(self))
+        indices = range(s.start, s.stop, s.step if s.step is not None else 1)
+        for idx in indices:
+            sample = self.metadata.iloc[idx].to_dict()
             id = sample['SAMPLE_KEY']
+            print(id)
             frames = self.load_img(id)
-            
-            yield {'id': id, 'frames': frames, 'meta': sample}
+            result = {'id': id, 'frames': frames, 'meta': sample}
+            yield result
 
     def load_img(self, key):
-        img = np.load(os.path.join(self.datadir, "%s.npz" % key))
+        img = np.load(os.path.join(self.datadir, key+".npz"))
         img = img["sample"] # Shape 520 x 696 x 5
         img = [self.transforms(img[:,:,idx]) for idx in range(5)]
-
         return img
 
 if __name__ == "__main__":
-    adapter = MolChemAdapter(datadir='data/images/', metafile=['data/metadata/datasplit1-train.csv', 
-                                                               'data/metadata/datasplit1-val.csv', 
-                                                               'data/metadata/datasplit1-test.csv'])
+    adapter = MolChemAdapter(datadir='../data/images/', metafile=['../data/metadata/datasplit1-train.csv', 
+                                                               '../data/metadata/datasplit1-val.csv', 
+                                                               '../data/metadata/datasplit1-test.csv'])
     ingestor = GulpIngestor(adapter,
-                            output_folder='data/gulpio/',
+                            output_folder='../data/gulpio/',
                             videos_per_chunk=1000,
-                            num_workers=16)
+                            num_workers=1)
     ingestor()
