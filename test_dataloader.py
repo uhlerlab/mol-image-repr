52efa44bc2bf6a5319c046c31782ea5d009b837f
Karren Dai Yang <karren@mit.edu>
2020-01-03 18:16:31 -0500

    added gulpio dataloader

--- test_dataloader.py
+++ test_dataloader.py
@@ -1,4 +1,5 @@
 from dataset.dataloader import MolImageMismatchDataset
+from dataset.gulpio_loader import MolImageMismatchGulpDataset
 
 def test_dataset(mode):
     dataset = MolImageMismatchDataset(datadir='data/images/',
@@ -6,17 +7,27 @@ def test_dataset(mode):
                               mode=mode)
 
     sample = dataset[0]
-    
-    for k in sample.keys():
-        print(k)
-        try:
-            print(sample[k].shape)
-        except:
-            print(sample[k])
+    for idx in range(100):
+        sample = dataset[idx]
+        print(sample['img'].shape)
+        print(sample['smile'])
 
-    return dataset
+def test_gulp_dataset(mode):
+    dataset = MolImageMismatchGulpDataset(datadir='data/gulpio/',
+                              metafile='data/metadata/datasplit1-test.csv',
+                              mode=mode)
+
+    sample = dataset[0]
+    for idx in range(100):
+        sample = dataset[idx]
+        print(sample['img'].shape)
+        print(sample['smile'])
 
 # test code
 if __name__ == '__main__':
+    start = time.time()
     test_dataset(mode='train')
-    test_dataset(mode='test')
+    print(time.time()-start)
+    start = time.time()
+    test_gulp_dataset(mode='train')
+    print(time.time()-start)
