15dcc0b20fc0aa3b881f7ce5e432a3c4ede8b690
Karren Yang <karren@mit.edu>
2020-01-06 00:47:45 -0500

    changed defaults

--- utils.py
+++ utils.py
@@ -9,22 +9,23 @@ def setup_args():
 
     options = argparse.ArgumentParser()
     # save and directory options
-    options.add_argument('--datadir', action="store", default="data/images/")
-    #options.add_argument('--datadir', action="store", default="data/gulpio/")
+    #options.add_argument('--datadir', action="store", default="data/images/")
+    options.add_argument('--datadir', action="store", default="data/gulpio/")
     options.add_argument('--train-metafile', action="store", default="data/metadata/datasplit1-train.csv")
     options.add_argument('--val-metafile', action="store", default="data/metadata/datasplit1-val.csv")
     options.add_argument('--save-dir', action="store", default='results/test/')
     options.add_argument('--save-freq', action="store", default=1, type=int)
+    options.add_argument('--seed', action="store", default=42, type=int)
 
     # model parameters
     options.add_argument('--model', action="store", dest="model", default='molimagenetclass')
-    options.add_argument('--dataset', action="store", dest="dataset", default='mismatch')
+    options.add_argument('--dataset', action="store", dest="dataset", default='mismatch-gulp')
     options.add_argument('--optimizer', action="store", dest="optimizer", default='sgd')
     options.add_argument('--scheduler', action="store", dest="scheduler", default='step')
 
     # training parameters
     options.add_argument('--batch-size', action="store", dest="batch_size", default=64, type=int)
-    options.add_argument('--num-workers', action="store", dest="num_workers", default=10, type=int)
+    options.add_argument('--num-workers', action="store", dest="num_workers", default=8, type=int)
     options.add_argument('-lr', '--learning-rate', action="store", dest="learning_rate", default=1e-2, type=float)
     options.add_argument('--max-epochs', action="store", dest="max_epochs", default=1000, type=int)
     options.add_argument('--weight-decay', action="store", dest="weight_decay", default=1e-5, type=float)
